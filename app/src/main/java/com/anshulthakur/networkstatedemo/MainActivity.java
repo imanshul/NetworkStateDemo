fcba0c2d734ce42394609750cdb37f8017ba75f3
imanshul <anshulthakur939@gmail.com>
2019-04-30 18:38:45 +0530

    Network State Checker v1.0

--- app/src/main/java/com/anshulthakur/networkstatedemo/MainActivity.java
+++ app/src/main/java/com/anshulthakur/networkstatedemo/MainActivity.java
@@ -3,11 +3,22 @@ package com.anshulthakur.networkstatedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anshulthakur.networkstatechecker.InternetStateChecker;
+
 public class MainActivity extends AppCompatActivity {
 
+    InternetStateChecker checker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checker = new InternetStateChecker(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        checker.onDestroy();
     }
 }
